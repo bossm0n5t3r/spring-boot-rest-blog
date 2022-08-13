11443d96b0e331b74ffd9187124e4a6d92859c4c
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-08-14 05:20:40 +0900

    feat: Implement ArticleService

--- src/main/kotlin/me/bossm0n5t3r/blog/article/application/ArticleService.kt
+++ src/main/kotlin/me/bossm0n5t3r/blog/article/application/ArticleService.kt
@@ -4,6 +4,8 @@ import me.bossm0n5t3r.blog.article.application.dto.CreateArticleDto
 import me.bossm0n5t3r.blog.article.application.dto.UpdateArticleDto
 import me.bossm0n5t3r.blog.article.domain.Article
 import me.bossm0n5t3r.blog.article.domain.ArticleRepository
+import me.bossm0n5t3r.blog.common.exception.ErrorMessage
+import me.bossm0n5t3r.blog.common.exception.ResourceNotFoundException
 import org.springframework.stereotype.Service
 
 @Service
@@ -11,18 +13,23 @@ class ArticleService(
     private val articleRepository: ArticleRepository
 ) {
     fun createArticle(dto: CreateArticleDto) {
-        TODO("Not yet implemented")
+        dto.validate()
+        articleRepository.save(Article(dto))
     }
 
     fun findById(id: Long): Article? {
-        TODO("Not yet implemented")
+        return articleRepository.findById(id).orElse(null)
     }
 
     fun updateArticle(dto: UpdateArticleDto) {
-        TODO("Not yet implemented")
+        val article = articleRepository.findById(dto.id).orElseThrow {
+            ResourceNotFoundException(ErrorMessage.NOT_FOUND_ARTICLE_BY_ID.message)
+        }
+        dto.validate()
+        article.updateArticle(dto)
     }
 
     fun deleteById(id: Long) {
-        TODO("Not yet implemented")
+        articleRepository.deleteById(id)
     }
 }
