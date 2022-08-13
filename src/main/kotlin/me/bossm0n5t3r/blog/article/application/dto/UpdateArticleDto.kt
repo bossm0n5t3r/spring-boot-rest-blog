437b6c4fb20d2ad45bbab5ed4942e25507a267f2
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-08-14 05:15:15 +0900

    feat: Add validate in Create, UpdateArticleDto

--- src/main/kotlin/me/bossm0n5t3r/blog/article/application/dto/UpdateArticleDto.kt
+++ src/main/kotlin/me/bossm0n5t3r/blog/article/application/dto/UpdateArticleDto.kt
@@ -1,7 +1,14 @@
 package me.bossm0n5t3r.blog.article.application.dto
 
+import me.bossm0n5t3r.blog.common.exception.ErrorMessage
+import me.bossm0n5t3r.blog.common.exception.ResourceNotFoundException
+
 data class UpdateArticleDto(
     val id: Long = 0L,
     val subject: String? = null,
     val content: String? = null,
-)
+) {
+    fun validate() {
+        if (subject.isNullOrBlank()) throw ResourceNotFoundException(ErrorMessage.SUBJECT_IS_BLANK.message)
+    }
+}
