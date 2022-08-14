05d3321d72fdccfdc01da39d51941fd0b4342a45
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-08-14 19:55:30 +0900

    feat: Use InvalidException in ArticleDtos
    - Edit CreateArticleDto, UpdateArticleDto
      - Replace ResourceNotFoundException with InvalidException

--- src/main/kotlin/me/bossm0n5t3r/blog/article/application/dto/CreateArticleDto.kt
+++ src/main/kotlin/me/bossm0n5t3r/blog/article/application/dto/CreateArticleDto.kt
@@ -1,14 +1,14 @@
 package me.bossm0n5t3r.blog.article.application.dto
 
 import me.bossm0n5t3r.blog.common.exception.ErrorMessage
-import me.bossm0n5t3r.blog.common.exception.ResourceNotFoundException
+import me.bossm0n5t3r.blog.common.exception.InvalidException
 
 data class CreateArticleDto(
     val subject: String,
     val content: String,
 ) {
     fun validate() {
-        if (subject.isBlank()) throw ResourceNotFoundException(ErrorMessage.SUBJECT_IS_BLANK.message)
-        if (content.isBlank()) throw ResourceNotFoundException(ErrorMessage.CONTENT_IS_BLANK.message)
+        if (subject.isBlank()) throw InvalidException(ErrorMessage.SUBJECT_IS_BLANK.message)
+        if (content.isBlank()) throw InvalidException(ErrorMessage.CONTENT_IS_BLANK.message)
     }
 }
--- src/main/kotlin/me/bossm0n5t3r/blog/article/application/dto/UpdateArticleDto.kt
+++ src/main/kotlin/me/bossm0n5t3r/blog/article/application/dto/UpdateArticleDto.kt
@@ -1,13 +1,13 @@
 package me.bossm0n5t3r.blog.article.application.dto
 
 import me.bossm0n5t3r.blog.common.exception.ErrorMessage
-import me.bossm0n5t3r.blog.common.exception.ResourceNotFoundException
+import me.bossm0n5t3r.blog.common.exception.InvalidException
 
 data class UpdateArticleDto(
     val subject: String? = null,
     val content: String? = null,
 ) {
     fun validate() {
-        if (subject.isNullOrBlank()) throw ResourceNotFoundException(ErrorMessage.SUBJECT_IS_BLANK.message)
+        if (subject.isNullOrBlank()) throw InvalidException(ErrorMessage.SUBJECT_IS_BLANK.message)
     }
 }
