23abc0d6b9363b7067b06d9969be87089a9d9df2
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-08-16 00:31:59 +0900

    feat: Add Comment

---
+++ src/main/kotlin/me/bossm0n5t3r/blog/comment/domain/Comment.kt
@@ -0,0 +1,32 @@
+package me.bossm0n5t3r.blog.comment.domain
+
+import me.bossm0n5t3r.blog.article.domain.Article
+import me.bossm0n5t3r.blog.common.domain.BaseEntity
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.Table
+
+@Entity
+@Table(name = "comment_info")
+class Comment(
+    content: String,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id", nullable = false, updatable = false)
+    var article: Article,
+
+) : BaseEntity<Long>() {
+    @Column(name = "content", nullable = false, updatable = true)
+    var content: String = content
+
+    fun updateComment(updateCommentDto: UpdateCommentDto) {
+        this.content = updateCommentDto.content
+    }
+}
+
+data class UpdateCommentDto(
+    val content: String
+)
