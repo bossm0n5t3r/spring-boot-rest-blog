bf892d465f80d29ad0a2ba8077802f6f3f95528f
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-08-15 22:24:20 +0900

    test: Edit ArticleControllerTest
    - Add missing mock
    - Add "should_404_response_if_invalid_create_article_request"
    - Add "should_201_response_if_create_article"
    - Edit wrong expect status

--- src/test/kotlin/me/bossm0n5t3r/blog/article/presentation/ArticleControllerTest.kt
+++ src/test/kotlin/me/bossm0n5t3r/blog/article/presentation/ArticleControllerTest.kt
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
 import io.mockk.justRun
-import io.mockk.mockkClass
 import io.mockk.verify
 import me.bossm0n5t3r.blog.article.application.ArticleService
+import me.bossm0n5t3r.blog.article.application.dto.CreateArticleDto
 import me.bossm0n5t3r.blog.article.application.dto.UpdateArticleDto
 import me.bossm0n5t3r.blog.article.presentation.dto.ArticleDto
 import me.bossm0n5t3r.blog.common.CommonUtil
@@ -20,6 +20,7 @@ import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
@@ -39,6 +40,8 @@ internal class ArticleControllerTest {
 
     @Test
     fun should_200_response_if_request_get_all_articles() {
+        every { articleService.findAll() } returns emptyList()
+
         mockMvc.perform(
             get("/articles")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -63,7 +66,14 @@ internal class ArticleControllerTest {
     @Test
     fun should_200_response_if_request_valid_article_id() {
         val validArticleId = faker.number().randomNumber()
-        every { articleService.findById(validArticleId) } returns mockkClass(ArticleDto::class)
+        every { articleService.findById(validArticleId) } returns
+            ArticleDto(
+                id = validArticleId,
+                subject = faker.lorem().characters(),
+                content = faker.lorem().characters(),
+                hits = faker.number().randomDigit(),
+                recommend = faker.number().randomDigit()
+            )
 
         mockMvc.perform(
             get("/articles/$validArticleId")
@@ -74,7 +84,36 @@ internal class ArticleControllerTest {
     }
 
     @Test
-    fun should_400_response_if_invalid_update_article_request() {
+    fun should_404_response_if_invalid_create_article_request() {
+        val dto = CreateArticleDto(subject = "", content = faker.lorem().characters())
+        every { articleService.createArticle(dto) } throws
+            ResourceNotFoundException(ErrorMessage.SUBJECT_IS_BLANK.message)
+
+        mockMvc.perform(
+            post("/articles")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dto))
+        )
+            .andExpect(status().isNotFound)
+    }
+
+    @Test
+    fun should_201_response_if_create_article() {
+        val dto = CreateArticleDto(
+            subject = faker.lorem().characters(), content = faker.lorem().characters()
+        )
+        justRun { articleService.createArticle(dto) }
+
+        mockMvc.perform(
+            post("/articles")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dto))
+        )
+            .andExpect(status().isCreated)
+    }
+
+    @Test
+    fun should_404_response_if_invalid_update_article_request() {
         val articleId = faker.number().randomNumber()
         val dto = UpdateArticleDto(subject = null, content = faker.lorem().characters())
         every { articleService.updateArticle(articleId, dto) } throws
@@ -85,7 +124,7 @@ internal class ArticleControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(dto))
         )
-            .andExpect(status().isBadRequest)
+            .andExpect(status().isNotFound)
     }
 
     @Test
