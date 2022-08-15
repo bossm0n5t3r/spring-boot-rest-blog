7e48856b1c1bf443b3891e7edd64c5009c633b86
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-08-16 04:07:20 +0900

    feat: Edit ErrorMessage
    - Add NOT_FOUND_COMMENT

--- src/main/kotlin/me/bossm0n5t3r/blog/common/exception/ErrorMessage.kt
+++ src/main/kotlin/me/bossm0n5t3r/blog/common/exception/ErrorMessage.kt
@@ -4,4 +4,5 @@ enum class ErrorMessage(val message: String) {
     SUBJECT_IS_BLANK("Subject is blank."),
     CONTENT_IS_BLANK("Content is blank."),
     NOT_FOUND_ARTICLE_BY_ID("There are no articles for this id."),
+    NOT_FOUND_COMMENT("There is no comment."),
 }
