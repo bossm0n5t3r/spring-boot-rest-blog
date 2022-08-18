758af06e233a9fc6bdceebb8cd50b0892823ee92
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-08-18 19:15:30 +0900

    test: Add DirtiesContext annotation
    - Edit ArticleRepositoryTest, CommentRepositoryTest

--- src/test/kotlin/me/bossm0n5t3r/blog/article/domain/ArticleRepositoryTest.kt
+++ src/test/kotlin/me/bossm0n5t3r/blog/article/domain/ArticleRepositoryTest.kt
@@ -9,8 +9,10 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+import org.springframework.test.annotation.DirtiesContext
 
 @DataJpaTest
+@DirtiesContext
 class ArticleRepositoryTest {
     @Autowired
     lateinit var entityManager: TestEntityManager
