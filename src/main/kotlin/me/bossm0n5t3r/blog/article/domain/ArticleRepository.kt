8fd1dc66d0d71eb5db0c6480fc34b729b437912e
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-08-18 17:13:09 +0900

    feat: Update ArticleRepository
    - Add missing Repository annotation
    - Add JpaSpecificationExecutor interface
    - Add findAllByOrderByCreatedAtDesc

--- src/main/kotlin/me/bossm0n5t3r/blog/article/domain/ArticleRepository.kt
+++ src/main/kotlin/me/bossm0n5t3r/blog/article/domain/ArticleRepository.kt
@@ -1,5 +1,10 @@
 package me.bossm0n5t3r.blog.article.domain
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.stereotype.Repository
 
-interface ArticleRepository : JpaRepository<Article, Long>
+@Repository
+interface ArticleRepository : JpaRepository<Article, Long>, JpaSpecificationExecutor<Article> {
+    fun findAllByOrderByCreatedAtDesc(): List<Article>
+}
