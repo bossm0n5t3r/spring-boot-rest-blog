777b8c0cb431e7b7808c078ad33194c16821f8e5
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-08-18 17:49:03 +0900

    chore: Edit Constants
    - Delete RecentArticles.TIMEOUT

--- src/main/kotlin/me/bossm0n5t3r/blog/common/configuration/Constants.kt
+++ src/main/kotlin/me/bossm0n5t3r/blog/common/configuration/Constants.kt
@@ -1,11 +1,8 @@
 package me.bossm0n5t3r.blog.common.configuration
 
-import java.time.Duration
-
 object Constants {
     object RecentArticles {
         const val RedisKey = "blog-recent-articles"
         const val MAX_RECENT_ARTICLES_COUNT = 20
-        val TIMEOUT: Duration = Duration.ofHours(3L)
     }
 }
