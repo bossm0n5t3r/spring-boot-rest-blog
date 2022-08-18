b081b8817d0e08b3f38e8440645bf4490685b943
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-08-18 17:13:50 +0900

    feat: Update CommentRepository
    - Add missing Repository annotation
    - Add JpaSpecificationExecutor interface

--- src/main/kotlin/me/bossm0n5t3r/blog/comment/domain/CommentRepository.kt
+++ src/main/kotlin/me/bossm0n5t3r/blog/comment/domain/CommentRepository.kt
@@ -1,8 +1,11 @@
 package me.bossm0n5t3r.blog.comment.domain
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.stereotype.Repository
 
-interface CommentRepository : JpaRepository<Comment, Long> {
+@Repository
+interface CommentRepository : JpaRepository<Comment, Long>, JpaSpecificationExecutor<Comment> {
     fun findAllByArticleId(articleId: Long): List<Comment>
     fun findByArticleIdAndId(articleId: Long, id: Long): Comment?
     fun deleteByArticleIdAndId(articleId: Long, id: Long)
