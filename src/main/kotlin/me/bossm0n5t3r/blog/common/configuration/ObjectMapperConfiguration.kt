617d6e71601995b0c7352d3325196d782b8d3892
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-08-18 11:01:51 +0900

    fix: Edit ObjectMapperConfiguration
    - Register Jdk8Module

--- src/main/kotlin/me/bossm0n5t3r/blog/common/configuration/ObjectMapperConfiguration.kt
+++ src/main/kotlin/me/bossm0n5t3r/blog/common/configuration/ObjectMapperConfiguration.kt
@@ -1,12 +1,13 @@
 package me.bossm0n5t3r.blog.common.configuration
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class ObjectMapperConfiguration {
     @Bean
-    fun objectMapper() = ObjectMapper().registerKotlinModule()
+    fun objectMapper(): ObjectMapper = jacksonObjectMapper().registerModule(Jdk8Module())
 }
