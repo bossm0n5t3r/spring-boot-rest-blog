3cd98b91ae86fa519f05081af4970a2e50095ab1
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-08-18 17:18:30 +0900

    feat: Edit ArticleService
    - Add stringRedisTemplate, objectMapper
    - Add getRecentArticles method

--- src/main/kotlin/me/bossm0n5t3r/blog/article/application/ArticleService.kt
+++ src/main/kotlin/me/bossm0n5t3r/blog/article/application/ArticleService.kt
@@ -1,17 +1,23 @@
 package me.bossm0n5t3r.blog.article.application
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import me.bossm0n5t3r.blog.article.application.dto.CreateArticleDto
 import me.bossm0n5t3r.blog.article.application.dto.UpdateArticleDto
 import me.bossm0n5t3r.blog.article.domain.Article
 import me.bossm0n5t3r.blog.article.domain.ArticleRepository
 import me.bossm0n5t3r.blog.article.presentation.dto.ArticleDto
+import me.bossm0n5t3r.blog.common.configuration.Constants.RecentArticles
+import me.bossm0n5t3r.blog.common.configuration.readValueWithTypeReference
 import me.bossm0n5t3r.blog.common.exception.ErrorMessage
 import me.bossm0n5t3r.blog.common.exception.ResourceNotFoundException
+import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.stereotype.Service
 
 @Service
 class ArticleService(
-    private val articleRepository: ArticleRepository
+    private val articleRepository: ArticleRepository,
+    private val stringRedisTemplate: StringRedisTemplate,
+    private val objectMapper: ObjectMapper,
 ) {
     fun findAll(): List<ArticleDto> {
         return articleRepository.findAll().map { it.toDto() }
@@ -38,5 +44,24 @@ class ArticleService(
 
     fun deleteById(id: Long) {
         articleRepository.deleteById(id)
+    }
+
+    fun getRecentArticles(): List<ArticleDto> {
+        val readOpsForValue = stringRedisTemplate.opsForValue()
+        val recentArticlesInRedis = readOpsForValue.get(RecentArticles.RedisKey)
+        return if (recentArticlesInRedis != null) {
+            objectMapper.readValueWithTypeReference(recentArticlesInRedis)
+        } else {
+            articleRepository.findAllByOrderByCreatedAtDesc()
+                .take(RecentArticles.MAX_RECENT_ARTICLES_COUNT)
+                .map { it.toDto() }
+                .also {
+                    readOpsForValue.set(
+                        RecentArticles.RedisKey,
+                        objectMapper.writeValueAsString(it),
+                        RecentArticles.TIMEOUT,
+                    )
+                }
+        }
     }
 }
