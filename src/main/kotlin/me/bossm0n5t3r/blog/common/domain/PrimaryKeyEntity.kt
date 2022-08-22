b7e599b5e8a153ae44c82ad8adf4dac292c893ab
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-08-22 21:00:00 +0900

    feat: Add PrimaryKeyEntity

---
+++ src/main/kotlin/me/bossm0n5t3r/blog/common/domain/PrimaryKeyEntity.kt
@@ -0,0 +1,54 @@
+package me.bossm0n5t3r.blog.common.domain
+
+import org.hibernate.proxy.HibernateProxy
+import org.springframework.data.domain.Persistable
+import java.io.Serializable
+import java.util.Objects
+import java.util.UUID
+import javax.persistence.Column
+import javax.persistence.Id
+import javax.persistence.MappedSuperclass
+import javax.persistence.PostLoad
+import javax.persistence.PostPersist
+
+@MappedSuperclass
+abstract class PrimaryKeyEntity : Persistable<UUID> {
+    @Id
+    @Column(columnDefinition = "uuid")
+    private val id: UUID = UUID.randomUUID() // TODO Use ULID later
+
+    @Transient
+    private var _isNew = true
+
+    override fun getId(): UUID = id
+
+    override fun isNew(): Boolean = _isNew
+
+    override fun equals(other: Any?): Boolean {
+        if (other == null) {
+            return false
+        }
+
+        if (other !is HibernateProxy && this::class != other::class) {
+            return false
+        }
+
+        return id == getIdentifier(other)
+    }
+
+    private fun getIdentifier(obj: Any): Serializable {
+        return if (obj is HibernateProxy) {
+            obj.hibernateLazyInitializer.identifier
+        } else {
+            (obj as PrimaryKeyEntity).id
+        }
+    }
+
+    override fun hashCode() = Objects.hashCode(id)
+
+    @PostPersist
+    @PostLoad
+    protected fun load() {
+        _isNew = false
+    }
+}
