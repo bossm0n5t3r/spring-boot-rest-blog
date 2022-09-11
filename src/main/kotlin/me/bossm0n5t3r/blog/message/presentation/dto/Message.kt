4ea31e4ccc432b0f9f70da746f9dfbf55a33ea7e
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-09-11 21:00:00 +0900

    feat: Edit MessageDto
    - Rename and add topic property

--- src/main/kotlin/me/bossm0n5t3r/blog/message/presentation/dto/Message.kt
+++
@@ -1,5 +0,0 @@
-package me.bossm0n5t3r.blog.message.presentation.dto
-
-data class Message(
-    val content: String
-)
---
+++ src/main/kotlin/me/bossm0n5t3r/blog/message/presentation/dto/MessageDto.kt
@@ -0,0 +1,6 @@
+package me.bossm0n5t3r.blog.message.presentation.dto
+
+data class MessageDto(
+    val topic: String,
+    val message: String,
+)
