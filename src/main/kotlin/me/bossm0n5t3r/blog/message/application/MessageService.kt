dc23118cb79d2e2b3de2ea1a1903036e727d1d63
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-09-12 03:54:30 +0900

    chore: Edit MessageService
    - Rename Message to T

--- src/main/kotlin/me/bossm0n5t3r/blog/message/application/MessageService.kt
+++ src/main/kotlin/me/bossm0n5t3r/blog/message/application/MessageService.kt
@@ -2,7 +2,7 @@ package me.bossm0n5t3r.blog.message.application
 
 import org.apache.kafka.clients.consumer.ConsumerRecord
 
-interface MessageService<Message, K, V> {
-    fun sendMessage(messageDto: Message)
+interface MessageService<T, K, V> {
+    fun sendMessage(messageDto: T)
     fun receiveMessage(record: ConsumerRecord<K, V>)
 }
